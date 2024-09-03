1c83953802d6ad64c1f8353ccc93afd680402367
Erfan Qasemizade <soulburn007@gmail.com>
2024-09-03 19:37:03 +0330

    add product detail page (WIP)

--- src/components/molecules/product-list-item.tsx
+++ src/components/molecules/product-list-item.tsx
@@ -1,7 +1,6 @@
-import { AddButton } from "../atoms/add-button";
-
 export type ProductData = {
   id: string;
+  slug: string;
   image: string;
   name: string;
   price: number;
@@ -13,23 +12,20 @@ type ProductProps = {
 
 export function ProductListItem({ product }: ProductProps) {
   return (
-    <div className="">
+    <a href={`/shop/${product.slug}`} className="group">
       <div className="overflow-hidden rounded-md h-96">
         <img
           src={product.image}
           alt={product.name}
-          className="object-cover w-full transition duration-500 rounded-md h-96 hover:scale-105"
+          className="object-cover w-full transition duration-500 rounded-md h-96 group-hover:scale-105"
         />
       </div>
       <div className="flex items-center justify-between mt-2">
-        <div>
-          <h3 className="text-sm font-semibold">{product.name}</h3>
-          <h4 className="text-xs text-muted-foreground">
-            ${product.price.toFixed(2)}
-          </h4>
-        </div>
-        <AddButton />
+        <h3 className="text-sm font-semibold">{product.name}</h3>
+        <h4 className="text-xs text-muted-foreground">
+          ${product.price.toFixed(2)}
+        </h4>
       </div>
-    </div>
+    </a>
   );
 }
--- src/components/organizations/product-list.tsx
+++ src/components/organizations/product-list.tsx
@@ -14,7 +14,7 @@ export function ProductList({ heading, subheading, products }: Props) {
     <section className="container">
       <h2 className="text-2xl font-semibold font-display">{heading}</h2>
       <h3 className="mt-1 text-sm text-muted-foreground">{subheading}</h3>
-      <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-cols-1 gap-8 lg:gap-10 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {products.map((product) => (
           <ProductListItem key={product.id} product={product} />
         ))}
